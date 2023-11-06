64257c044a49d00f88f41a5f998735aacab90951
daniel.akinmaiyejolu <daniel.akinmaiyejolu@avanade.com>
2023-11-06 14:13:54 +0000

    Code along

---
+++ CodeAlong_operators/index.js
@@ -0,0 +1,30 @@
+console.log(3+5);
+console.log("my mum: " + 6);
+console.log(`my mum = ${3+3}`);
+
+//STANDARD OPERATORS
+console.log(8+5);
+console.log(8-5);
+console.log(8/5);
+console.log(8*5);
+
+let a = 5
+console.log(a);
+
+a = a + 5
+console.log(a);
+
+a += 5 
+console.log(a);
+
+a *= 5
+console.log(a);
+
+console.log(2 ** 3);
+
+console.log(2 / 3);
+
+a++
+console.log(a);
+a--
+console.log(a);
