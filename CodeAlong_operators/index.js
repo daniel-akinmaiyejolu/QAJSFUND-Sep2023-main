584fc8359d03fa181a2afb7167185c5fc407c1a8
daniel.akinmaiyejolu <daniel.akinmaiyejolu@avanade.com>
2023-11-06 14:29:12 +0000

    first commit

--- CodeAlong_operators/index.js
+++ CodeAlong_operators/index.js
@@ -28,3 +28,22 @@ a++
 console.log(a);
 a--
 console.log(a);
+
+console.log(10 % 2)  // give the remainder
+// BIDMAS
+
+console.log(3 > 2);
+console.log(3 < 2);
+console.log(3 <= 2);
+
+console.log(3 == 4); // comparison
+console.log(3 === 3); // strict comparison (value and type)
+console.log(3 != 4); // Is not equal to
+
+let c = true
+let b = false
+
+console.log(c && b); // c and b
+console.log(c || b); // or operator
+console.log(c && ! b); // c and not b
+
